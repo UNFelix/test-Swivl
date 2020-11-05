b2018d3cc84388ca3b62f5baeaea0e7b0aa19cad
unfelix <shevko.evheniy@gmail.com>
2020-11-05 11:56:22 +0200

    js dropdown

--- app/js/main.js
+++ app/js/main.js
@@ -1,6 +1,6 @@
-$(function(){
+const dropdown = document.getElementsByClassName('dropdown')[0]
+dropdown.addEventListener('click', toggleDropdown)
 
-
-
-  
-});
+function toggleDropdown(e) {
+  dropdown.classList.toggle("open")
+}
