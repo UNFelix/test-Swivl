0e491f95a5f91dc97a13db9197670060277ca3a3
unfelix <shevko.evheniy@gmail.com>
2021-11-13 20:49:03 +0200

    remake

--- app/js/main.js
+++
@@ -1,9 +0,0 @@
-const dropdown = document.getElementsByClassName('dropdown')[0]
-dropdown.addEventListener('click', toggleDropdown)
-
-function toggleDropdown(e) {
-  dropdown.classList.toggle("open")
-}
-
-const toggler = document.getElementsByClassName('toggler')[0]
-toggler.onclick = () => toggler.classList.toggle('on')
